b91d69eadd92e68eee151b815aa760f751d151f6
akhilkotturi <akhil.kotturi@gmail.com>
2025-04-04 22:40:59 -0500

    hide ids

--- src/fireBaseConfig.js
+++ src/fireBaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAUBg0R7N2iTK5FOQICgjvzxhjgfBinXs4",
-    authDomain: "ucode-python-lessons.firebaseapp.com",
-    projectId: "ucode-python-lessons",
-    storageBucket: "ucode-python-lessons.firebasestorage.app",
-    messagingSenderId: "1066121844994",
-    appId: "1:1066121844994:web:0519c4d8302a6730e50a1b",
-    measurementId: "G-TXDXW2608C"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
